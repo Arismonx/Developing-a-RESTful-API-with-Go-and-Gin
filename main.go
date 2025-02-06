fa279fbe8342e9dd76de9f2f7eb8e465338f06bd
Arismonx <arismonx@gmail.com>
2025-02-06 16:58:43 +0700

    handler to return a specific item

--- main.go
+++ main.go
@@ -36,11 +36,23 @@ func createNewAlbum(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 func main() {
 	router := gin.Default()
 
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", createNewAlbum)
+	router.GET("/albums/:id", getAlbumByID)
 
 	router.Run("localhost:8080")
 }
